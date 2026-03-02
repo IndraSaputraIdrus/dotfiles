d219a726f6efe8424beb052d9b4cc38be1713834
Indra Saputra Idrus <indrasaputraidrus@gmail.com>
2026-03-02 18:06:38 +0800

    nvim: remove bento & install mini.tabline

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,23 +1,23 @@
--- local gruvbox = {
---   -- source: https://github.com/NvChad/base46/blob/v3.0/lua/base46/themes/gruvbox.lua
---   base00 = "#282828",
---   base01 = "#3c3836",
---   base02 = "#423e3c",
---   base03 = "#484442",
---   base04 = "#bdae93",
---   base05 = "#d5c4a1",
---   base06 = "#ebdbb2",
---   base07 = "#fbf1c7",
---   base08 = "#fb4934",
---   base09 = "#fe8019",
---   base0A = "#fabd2f",
---   base0B = "#b8bb26",
---   base0C = "#8ec07c",
---   base0D = "#83a598",
---   base0E = "#d3869b",
---   base0F = "#d65d0e",
--- }
-
+-- -- local gruvbox = {
+-- --   -- source: https://github.com/NvChad/base46/blob/v3.0/lua/base46/themes/gruvbox.lua
+-- --   base00 = "#282828",
+-- --   base01 = "#3c3836",
+-- --   base02 = "#423e3c",
+-- --   base03 = "#484442",
+-- --   base04 = "#bdae93",
+-- --   base05 = "#d5c4a1",
+-- --   base06 = "#ebdbb2",
+-- --   base07 = "#fbf1c7",
+-- --   base08 = "#fb4934",
+-- --   base09 = "#fe8019",
+-- --   base0A = "#fabd2f",
+-- --   base0B = "#b8bb26",
+-- --   base0C = "#8ec07c",
+-- --   base0D = "#83a598",
+-- --   base0E = "#d3869b",
+-- --   base0F = "#d65d0e",
+-- -- }
+--
 local gruvchad = {
   -- source: https://github.com/NvChad/base46/blob/v3.0/lua/base46/themes/gruvchad.lua
   base00 = "#1e2122",
--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -20,6 +20,11 @@ return {
     config = function()
       require("mini.ai").setup()
       require("mini.surround").setup()
+
+      require("mini.tabline").setup()
+      vim.keymap.set("n", "H","<cmd>bprev<cr>", { desc = "Move to previous buffer" })
+      vim.keymap.set("n", "L","<cmd>bnex<cr>", { desc = "Move to next buffer" })
+      vim.keymap.set("n", "<leader>x", require("mini.bufremove").delete, { desc = "Delete current focus buffer" })
     end
   }
 }
